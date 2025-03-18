989adbcc9fa46fc1362b3f13671b6292d1feddb4
Quint-coding <kloq996@gmail.com>
2025-03-18 15:34:16 +0100

    hele tooltip weg

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,10 +30,10 @@ st.markdown(
 options = st.sidebar.radio('Visualisaties',
                            options =['Fietsdrukte kaart'])
 
-tooltip = {
-    "html": "<b>Busyness:</b> {busyness}",
-    "style": {"backgroundColor": "steelblue", "color": "white"},
-}
+# tooltip = {
+#     "html": "<b>Busyness:</b> {busyness}",
+#     "style": {"backgroundColor": "steelblue", "color": "white"},
+# }
 
 ViewState = pdk.ViewState(
             latitude=51.50853,
