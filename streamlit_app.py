84841e796326a80a8d292395323fa8e5b47f2892
Quint-coding <kloq996@gmail.com>
2025-03-18 16:05:42 +0100

    grotere radius

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ st.pydeck_chart(
                 "HexagonLayer",
                 data=df,
                 get_position="[Longitude, Latitude]",
-                radius=100,
+                radius=2000,
                 elevation_scale=5,
                 elevation_range=[0, 100],
                 pickable=True,
