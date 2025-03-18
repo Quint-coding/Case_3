fbdca3620d5cfa7f72b9e7a20e5b33aa9b203e0b
Quint-coding <kloq996@gmail.com>
2025-03-18 14:41:21 +0100

    radius = 1000

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,10 +30,10 @@ st.markdown(
 options = st.sidebar.radio('Visualisaties',
                            options =['Fietsdrukte kaart'])
 
-# tooltip = {
-#     "html": "<b>Busyness:</b> {busyness}",
-#     "style": {"backgroundColor": "steelblue", "color": "white"},
-# }
+tooltip = {
+    "html": "<b>Busyness:</b> {busyness}",
+    "style": {"backgroundColor": "steelblue", "color": "white"},
+}
 
 ViewState = pdk.ViewState(
             latitude=51.50853,
@@ -49,9 +49,9 @@ st.pydeck_chart(
         layers=[
             pdk.Layer(
                 "HexagonLayer",
-                data=df['traveler_count'],
+                data=df,
                 get_position="[Longitude, Latitude]",
-                radius=100,
+                radius=1000,
                 elevation_scale=5,
                 elevation_range=[0, 1000],
                 pickable=True,
@@ -59,7 +59,7 @@ st.pydeck_chart(
                 auto_highlight=True,
             ),
         ],
-        # tooltip = tooltip
+        tooltip = tooltip
     )
 )
 
