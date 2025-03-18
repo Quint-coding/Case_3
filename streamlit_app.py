2028cd8c12f0de1ab7e926b191064f8b8972b1dc
Quint-coding <kloq996@gmail.com>
2025-03-18 16:34:48 +0100

    scatterplot

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,17 +65,34 @@ tooltip = {
 #     )
 # )
 
+# layer = pdk.Layer(
+#     "ScatterplotLayer",
+#     data = df,
+#     get_position=["Longitude", "Latitude"],
+#     auto_highlight=True,
+#     elevation_scale=50,
+#     pickable=True,
+#     tooltip=True,
+#     elevation_range=[0, 3000],
+#     extruded=True,
+#     coverage=1,
+# )
+
 layer = pdk.Layer(
-    "HexagonLayer",
-    data = df,
-    get_position=["Longitude", "Latitude"],
-    auto_highlight=True,
-    elevation_scale=50,
+    "ScatterplotLayer",
+    df,
     pickable=True,
-    tooltip=True,
-    elevation_range=[0, 3000],
-    extruded=True,
-    coverage=1,
+    opacity=0.8,
+    stroked=True,
+    filled=True,
+    radius_scale=6,
+    radius_min_pixels=1,
+    radius_max_pixels=100,
+    line_width_min_pixels=1,
+    get_position="coordinates",
+    get_radius="exits_radius",
+    get_fill_color=[255, 140, 0],
+    get_line_color=[0, 0, 0],
 )
 
 # Set the viewport location
