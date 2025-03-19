cb986a911e7b428447675f1c4fcabea15a6cc299
Quint-coding <kloq996@gmail.com>
2025-03-19 15:29:21 +0100

    home page?

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,13 +69,14 @@ with st.container():
     st.markdown('<div class="subtitle">Interactieve visualisaties van fietsdrukte bij stations.</div>', unsafe_allow_html=True)
 
     # Optional image (replace with your own)
-    st.markdown('<div class="center"><img src="https://source.unsplash.com/1600x500/?bicycle,city" width="80%"></div>', unsafe_allow_html=True)
+    st.markdown('<div class="center"><img src="https://www.google.com/imgres?q=modern%20bicycle%20communte&imgurl=https%3A%2F%2Fimages.stockcake.com%2Fpublic%2Fd%2Fc%2F0%2Fdc093a70-8e92-4033-833a-04bdd52838ec_large%2Furban-biking-commute-stockcake.jpg&imgrefurl=https%3A%2F%2Fstockcake.com%2Fi%2Furban-biking-commute_862811_1051500&docid=T6GSjy4-IsrQ2M&tbnid=SPrkKlowkShmyM&vet=12ahUKEwiy8L6lrZaMAxWTgP0HHQJBESkQM3oECGIQAA..i&w=512&h=512&hcb=2&itg=1&ved=2ahUKEwiy8L6lrZaMAxWTgP0HHQJBESkQM3oECGIQAA" width="80%"></div>', unsafe_allow_html=True)
 
     # Navigation button
     if st.button("Ga naar Visualisaties 🚀"):
-        st.switch_page("app.py")  # Update this with the correct file name
+        st.switch_page("streamlit_app.py")  # Update this with the correct file name
 
     st.markdown('</div>', unsafe_allow_html=True)
+
 
 st.title("3D kaart van de fiets drukte op stations")
 
